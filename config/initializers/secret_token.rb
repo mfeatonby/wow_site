7b805a2272d75615d7e95eea93bbf4931a6bf4ed
Malcolm Featonby <mfeatonby@telkomsa.net>
2011-08-27 23:08:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WowSite::Application.config.secret_token = '0c63a0d761ad2fb4bf141408dce22a6a9a035e0e412091f561ef2ce4471d81fef897e549e447df5007d03e4aa525541cc32b59e5986fe1a457f1747c7f4dc3da'
